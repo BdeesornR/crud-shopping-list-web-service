3e28b7700b6c524f7d6af73949d84da23004553e
Bdeesorn Rukprayoon <Bdeesorn.ru@brightexpert.co>
2024-06-03 20:54:44 +0700

    Config Permission & Add Create user API

--- src/main/java/com/bdeesorn_r/shopping_list/services/UserService.java
+++ src/main/java/com/bdeesorn_r/shopping_list/services/UserService.java
@@ -1,8 +1,45 @@
 package com.bdeesorn_r.shopping_list.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
+
+import com.bdeesorn_r.shopping_list.dao.UserDao;
+import com.bdeesorn_r.shopping_list.dto.request.RequestUserDto;
+import com.bdeesorn_r.shopping_list.dto.response.ResponseCommonDto;
+import com.bdeesorn_r.shopping_list.repositories.UserRepo;
 
 @Service
 public class UserService {
-    //
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    private UserRepo userRepo;
+
+    public UserService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    public ResponseCommonDto createNewUser(RequestUserDto userDto) {
+        UserDao user = new UserDao();
+
+        user.setUsername(userDto.getUsername());
+        user.setEmail(userDto.getEmail());
+
+        String salt = BCrypt.gensalt();
+
+        user.setSalt(salt);
+        user.setHashedPassword(BCrypt.hashpw(userDto.getPassword(), salt));
+        user.setCreatedBy(userDto.getUsername());
+        user.setUpdatedBy(userDto.getUsername());
+
+        try {
+            userRepo.save(user);
+        } catch (Exception e) {
+            logger.error("createNewUser", e);
+            return new ResponseCommonDto("500", "Cannot create user");
+        }
+
+        return new ResponseCommonDto("200", "User created successfully");
+    }
 }
--- src/main/resources/db/migration/V1__create_table.sql
+++ src/main/resources/db/migration/V1__create_table.sql
@@ -5,9 +5,9 @@ SET search_path = shopping_list;
 CREATE TABLE IF NOT EXISTS shopping_list.sl_user (
     id SERIAL NOT NULL PRIMARY KEY,
     username VARCHAR(20) NOT NULL UNIQUE,
-    email VARCHAR(50),
-    salt BYTEA NOT NULL,
-    hashed_password BYTEA NOT NULL,
+    email VARCHAR(40),
+    salt VARCHAR(30) NOT NULL,
+    hashed_password VARCHAR(60) NOT NULL,
     created_by VARCHAR(20) NOT NULL,
     created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_by VARCHAR(20),
@@ -16,12 +16,44 @@ CREATE TABLE IF NOT EXISTS shopping_list.sl_user (
 
 CREATE TABLE IF NOT EXISTS shopping_list.sl_role (
     id SERIAL NOT NULL PRIMARY KEY,
-    fk_user_id INTEGER NOT NULL REFERENCES shopping_list.sl_user(id),
+    role_code VARCHAR(20) NOT NULL,
+    role_name VARCHAR(40) NOT NULL,
+    role_desc VARCHAR(100) NOT NULL,
+    created_by VARCHAR(20) NOT NULL,
+    created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_by VARCHAR(20),
+    updated_date TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS shopping_list.sl_module (
+    id SERIAL NOT NULL PRIMARY KEY,
     module_code VARCHAR(20) NOT NULL,
+    module_name VARCHAR(40) NOT NULL,
+    module_desc VARCHAR(100) NOT NULL,
+    created_by VARCHAR(20) NOT NULL,
+    created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_by VARCHAR(20),
+    updated_date TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS shopping_list.sl_role_module (
+    id SERIAL NOT NULL PRIMARY KEY,
+    fk_role_id INTEGER NOT NULL REFERENCES shopping_list.sl_role(id),
+    fk_module_id INTEGER NOT NULL REFERENCES shopping_list.sl_module(id),
     can_view VARCHAR(1) NOT NULL,
     can_create VARCHAR(1) NOT NULL,
     can_update VARCHAR(1) NOT NULL,
     can_delete VARCHAR(1) NOT NULL,
+    created_by VARCHAR(20) NOT NULL,
+    created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_by VARCHAR(20),
+    updated_date TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS shopping_list.sl_user_role (
+    id SERIAL NOT NULL PRIMARY KEY,
+    fk_user_id INTEGER NOT NULL REFERENCES shopping_list.sl_user(id),
+    fk_role_id INTEGER NOT NULL REFERENCES shopping_list.sl_role(id),
     created_by VARCHAR(20) NOT NULL,
     created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_by VARCHAR(20),
