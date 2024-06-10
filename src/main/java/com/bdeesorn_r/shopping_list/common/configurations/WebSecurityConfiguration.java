cb37e0c35cf7ddb35f91cd848755e19602c09c8a
BdeesornR <bdeesorn@gmail.com>
2024-06-11 00:10:47 +0700

    Config CORS and CSRF

--- src/main/java/com/bdeesorn_r/shopping_list/common/configurations/WebSecurityConfiguration.java
+++ src/main/java/com/bdeesorn_r/shopping_list/common/configurations/WebSecurityConfiguration.java
@@ -1,30 +1,39 @@
 package com.bdeesorn_r.shopping_list.common.configurations;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration {
     @Bean
+    protected CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
+            CorsConfiguration corsConfiguration = new CorsConfiguration();
+            corsConfiguration.addAllowedOrigin("http://localhost:3000");
+            corsConfiguration.addAllowedHeader("*");
+            corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS"));
+            return corsConfiguration;
+        };
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(authz -> authz.ignoringRequestMatchers("**"))
+            .cors(authz -> authz.configurationSource(corsConfigurationSource()))
+            .csrf(authz -> authz.ignoringRequestMatchers("users/show-all-user"))
             .authorizeHttpRequests(
                     authz -> authz.requestMatchers("**").permitAll().anyRequest().authenticated())
             .httpBasic(Customizer.withDefaults())
             .formLogin(Customizer.withDefaults());
         return http.build();
     }
-
-    // @Bean
-    // public SecurityFilterChain corsFilterChain(HttpSecurity http) throws Exception {
-    //     http.cors(Customizer.withDefaults());
-    //     http.addFilterBefore(null, UsernamePasswordAuthenticationFilter.class)
-    //     return http.build();
-    // }
 }
